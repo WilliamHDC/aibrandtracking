0c9704a13ae6181c87fdd01446e1d781079e7327
William <w@hollingworth.digital>
2025-01-08 21:45:19 +0100

    Changed to relative paths for db and schema imports

--- app/api/topics/[projectId]/[topicName]/queries/route.js
+++ app/api/topics/[projectId]/[topicName]/queries/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
-import { db } from '../../../../../../lib/db';
-import { topics } from '../../../../../../lib/schema';
+import { db } from '../../../../../lib/db';
+import { topics } from '../../../../../lib/schema';
 
 // POST to add new queries to a topic
 export async function POST(req, { params }) {
